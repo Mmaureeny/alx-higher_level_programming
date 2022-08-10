09840db9a4c12920540d076dd3516d962f7aa274
Mmaureeny <muthonimaureen4@outlook.com>
2022-08-10 01:59:08 -0700

    Introduction to SQL programming

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Script that delete a database
+--If the database doesn’t exist script should not fail
+DELETE DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/README.md
@@ -0,0 +1,4 @@
+What’s a database
+How to use MySQL functions
+What’s a relational database
+How to create a database in MySQL
